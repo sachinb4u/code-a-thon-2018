b1d419a0f4eea9873a9589e2eacf23bb67e18e65
Sachin Bhosale <s.bhosale@sap.com>
2018-06-24 15:35:51 +0530

    integrated saved model

--- server.py
+++ server.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask import request
 
 import json
+import pickle
 
 app = Flask(__name__)
+
+
+incidentLabels = ['NoIssue', 'DBConnectionIssue', 'InvoiceIssue', 'OrderIssue', 'CommunityIssue',
+                 'WorkspaceIssue' , 'NetworkIssue', 'CommunityHealthIssue' ]
 
 @app.route("/predict", methods=['POST'])
 def predict_handler():
@@ -15,5 +20,18 @@ def predict_handler():
 
 def predict_impl(input_json):
     # TODO: call actual machine learning predication function here...
+    model = load_model()
+    prediction = model.predict()
+    
+    predictionName = prediciton.apply(lambda x : inidentLabels[x])
+    
     return "todo"
+
+
+def load_model():
+    model_filename = 'model_v1.pk'
+    with open('models/'+filename ,'rb') as f:
+        loaded_model = pickle.load(f)
+        
+    return loaded_model
 
