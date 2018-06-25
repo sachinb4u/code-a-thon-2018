d538855e6c49e2a70318775ab710ea6cb6a8e0ff
Sachin Bhosale <s.bhosale@sap.com>
2018-06-25 17:28:26 +0530

    server updated for preprocessing

--- server.py
+++ server.py
@@ -1,32 +1,55 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 
+import pandas as pd
 import json
 import pickle
 
 app = Flask(__name__)
 
-
 incidentLabels = ['NoIssue', 'DBConnectionIssue', 'InvoiceIssue', 'OrderIssue', 'CommunityIssue',
                  'WorkspaceIssue' , 'NetworkIssue', 'CommunityHealthIssue' ]
 
+filename = 'model_v1.pk'
+
 @app.route("/predict", methods=['POST'])
 def predict_handler():
-    input_str = request.form["input"]
-    print(input_str)
-    input_json = json.loads(input_str)
-    print(input_json)
-    return predict_impl(input_json)
-
-def predict_impl(input_json):
-    # TODO: call actual machine learning predication function here...
-    model = load_model()
-    prediction = model.predict()
     
-    predictionName = prediciton.apply(lambda x : inidentLabels[x])
-    
-    return "todo"
+    try:
+        req_json = request.get_json()
+        print('Request JSON = \n', req_json)
+        req_json = str(req_json).replace("'", '"')
+        print('Request JSON Updated= \n', req_json)
+        
+        reqDf = pd.read_json(req_json)
+        
+        print(reqDf)
+        
+    except Exception as e:
+        raise e
 
+    if reqDf.empty :
+        return (bad_request())
+    else:
+        #Load the saved model
+        model = load_model()
+        
+        print("The model has been loaded...doing predictions now...")
+        
+        predictions = model.predict(reqDf)
+        predNames = pd.Series(predicitons).apply(lambda x : incidentTypes[x])
+        
+        responses = jsonify(predictions=predNames)
+    
+    return (responses)
+
+def bad_request():
+    bad_response = '{"error" : "bad request"}'
+    response1 = jsonify(bad_response)
+    response1.status_code = 400
+    
+    return response1
 
 def load_model():
     model_filename = 'model_v1.pk'
@@ -35,3 +58,7 @@ def load_model():
         
     return loaded_model
 
+if __name__ == "__main__" :
+    app.run(debug=True)
+    
+# app.run(debug=True)
